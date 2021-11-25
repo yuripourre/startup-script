69279a5c4c36eaec5048778489253ca52b457869
Yuri Pourre <yuripourre@gmail.com>
2021-11-25 13:25:35 -0800

    Add commands to download and install IntelliJ

--- java.sh
+++ java.sh
@@ -7,6 +7,14 @@ sudo dnf -y install maven ant
 # Install Java OpenJDK 14
 sudo dnf -y install java-14-openjdk-devel
 
+# Download and install IntelliJ
+cd ~/
+wget -O idea-IC.tar.gz https://download.jetbrains.com/idea/ideaIC-2021.2.3.tar.gz
+tar -xvzf idea-IC.tar.gz
+rm idea-IC.tar.gz
+mv idea-IC* idea-IC
+cd -
+
 # Google Style
 #wget https://raw.githubusercontent.com/google/styleguide/gh-pages/intellij-java-google-style.xml
 #https://gerrit-review.googlesource.com/Documentation/dev-intellij.html
