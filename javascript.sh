a4c30e3d160ea4b47c71e8da7f7a5418f394aa40
Yuri Pourre <yuripourre@gmail.com>
2022-11-03 22:54:29 -0400

    Upgrade nvm

--- javascript.sh
+++ javascript.sh
@@ -1,5 +1,5 @@
 # Install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 source ~/.bashrc
 
 # Install latest NPM
