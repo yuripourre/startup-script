a0d477e6a49c21a73d0059e76337dd72d5289277
Yuri Pourre <yuripourre@gmail.com>
2023-02-19 01:39:51 -0800

    Add instructions to install OBS Studio

--- multimedia.sh
+++ multimedia.sh
@@ -1,7 +1,7 @@
 # Audio and Video Codecs
 sudo dnf -y install gstreamer-plugins-bad gstreamer-plugins-bad-nonfree gstreamer-plugins-ugly gstreamer-ffmpeg gstreamer1-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-good gstreamer1-plugins-base gstreamer1 x264
 
-#Not working
+# Not working
 #sudo dnf -y install gstreamer-plugins-bad-free-extras
 
 # Video Player VLC
@@ -10,3 +10,9 @@ sudo dnf -y install vlc
 # Audio
 sudo dnf -y install pavucontrol jack-audio-connection-kit-dbus
 #sudo dnf -y ardour
+
+# Flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Install OBS Studio
+flatpak install flathub -y com.obsproject.Studio
