baed543ca374291543b05a8266ca6f578e86aae9
Yuri Pourre <yuripourre@gmail.com>
2025-11-22 12:15:04 -0800

    Remove libreoffice

--- shrink.sh
+++ shrink.sh
@@ -1,9 +1,8 @@
 # Remove evolution (email client)
 sudo dnf remove evolution
 
-# Reduce max memory for services
-sudo sed -i "/^SystemMaxUse=/c SystemMaxUse=50M" /etc/systemd/journald.conf
-sudo sed -i "/^#SystemMaxUse=/c SystemMaxUse=50M" /etc/systemd/journald.conf
+# Remove libreoffice suite
+sudo dnf remove libreoffice*
 
 # Disable PackageKit
 gsettings set org.gnome.software download-updates false
@@ -14,3 +13,8 @@ sudo sed -i "/^#KeepCache=/c KeepCache=false" /etc/PackageKit/PackageKit.conf
 
 # Disable lang files other than english
 grep -qF '%_install_langs   en:en_US:en_US.UTF-8' /etc/rpm/macros.lang  || echo '%_install_langs   en:en_US:en_US.UTF-8' | sudo tee --append /etc/rpm/macros.lang
+
+# Reduce max memory for services
+#sudo sed -i "/^SystemMaxUse=/c SystemMaxUse=50M" /etc/systemd/journald.conf
+#sudo sed -i "/^#SystemMaxUse=/c SystemMaxUse=50M" /etc/systemd/journald.conf
+
