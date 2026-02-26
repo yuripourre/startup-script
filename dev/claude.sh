dc8791724559ea49059542880b61ac00d81953c4
Yuri Pourre <yuripourre@gmail.com>
2026-02-25 17:44:13 -0800

    Add script to install claude code

---
+++ dev/claude.sh
@@ -0,0 +1,6 @@
+# Install Claude Code
+curl -fsSL https://claude.ai/install.sh | bash
+
+# Add alias for Claude Code
+echo 'alias yolo="claude --dangerously-skip-permissions"' >>~/.bashrc
+source ~/.bashrc
