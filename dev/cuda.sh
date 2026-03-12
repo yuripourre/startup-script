c589ff27d573584b0dc901860b4ac57c314c2099
Yuri Pourre <yuripourre@gmail.com>
2026-03-12 09:40:02 -0700

    Update CUDA script

--- dev/cuda.sh
+++ dev/cuda.sh
@@ -1,5 +1,8 @@
 # NVidia GPU
 
-sudo dnf install -y nvidia-drivers
-sudo dnf config-manager addrepo --from-repofile=https://developer.download.nvidia.com/compute/cuda/repos/fedora42/x86_64/cuda-fedora42.repo
-sudo dnf install cuda-toolkit
+sudo dnf config-manager addrepo --from-repofile https://developer.download.nvidia.com/compute/cuda/repos/fedora43/x86_64/cuda-fedora43.repo
+sudo dnf clean all
+sudo dnf -y install cuda-toolkit-13-2
+
+# Install NVidia Open Driver
+# sudo dnf -y install nvidia-open
